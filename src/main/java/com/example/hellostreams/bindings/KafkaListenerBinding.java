8d9138bb784d737e7450a7198e83ece31b8e657a
Reinaldo Hermosilla <ghost_cad@hotmail.com>
2021-06-24 09:49:23 -0300

    added bindings, services, and folder practice resources

--- src/main/java/com/example/hellostreams/bindings/KafkaListenerBinding.java
+++ src/main/java/com/example/hellostreams/bindings/KafkaListenerBinding.java
@@ -1,4 +1,9 @@
 package com.example.hellostreams.bindings;
 
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.cloud.stream.annotation.Input;
+
 public interface KafkaListenerBinding {
+    @Input("input-channel-1")
+    KStream<String,String> inputStream();
 }
